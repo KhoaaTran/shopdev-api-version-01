f1cae02cbcf132f6e80a937de86fb11a2bbdd9dc
Tran Duc Khoa <tdkhoa73@gmail.com>
2024-10-24 15:28:37 +0700

    feat: add api user

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -1,11 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/checkstatus', (req, res, next) => {
-    res.status(200).json({
-        status: 'success',
-        message: 'api ok'
-    })
-})
+router.get("/checkstatus", (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "api ok",
+  });
+});
+
+router.get("/api/users", (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "api ok",
+    metadata: [{ name: "test", age: 40 }],
+  });
+});
 
 module.exports = router;
